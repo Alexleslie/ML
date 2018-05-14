3cc02d32784a90bedccca36cf0a23605326ea106
Alexleslie <577045156@qq.com>
2018-05-14 11:25:24 +0800

    update

--- tensorflow-practice/get_start.py
+++ tensorflow-practice/get_start.py
@@ -28,8 +28,8 @@ feed forward + backprop = epoch
 
 """
 
-x = tf.placeholder(float, [None, 784])
-y = tf.placeholder(float)
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32)
 
 n_notes_h1 = 500
 n_notes_h2 = 500
@@ -41,17 +41,17 @@ n_classes = 10
 
 def neural_network_model(data):
 
-    hidden_layer_1 = {'weights': tf.Variable(tf.random_normal(784, n_notes_h1)),
-                      'biases': tf.Variable(tf.random_normal(n_notes_h1))}
+    hidden_layer_1 = {'weights': tf.Variable(tf.random_normal([784, n_notes_h1])),
+                      'biases': tf.Variable(tf.random_normal([n_notes_h1]))}
 
-    hidden_layer_2 = {'weights': tf.Variable(tf.random_normal(n_notes_h1, n_notes_h2)),
-                      'biases': tf.Variable(tf.random_normal(n_notes_h1))}
+    hidden_layer_2 = {'weights': tf.Variable(tf.random_normal([n_notes_h1, n_notes_h2])),
+                      'biases': tf.Variable(tf.random_normal([n_notes_h1]))}
 
-    hidden_layer_3 = {'weights': tf.Variable(tf.random_normal(n_notes_h2, n_notes_h3)),
-                      'biases': tf.Variable(tf.random_normal(n_notes_h1))}
+    hidden_layer_3 = {'weights': tf.Variable(tf.random_normal([n_notes_h2, n_notes_h3])),
+                      'biases': tf.Variable(tf.random_normal([n_notes_h1]))}
 
-    output_layer = {'weights': tf.Variable(tf.random_normal(n_notes_h3, n_classes)),
-                    'biases': tf.Variable(tf.random_normal(n_classes))}
+    output_layer = {'weights': tf.Variable(tf.random_normal([n_notes_h3, n_classes])),
+                    'biases': tf.Variable(tf.random_normal([n_classes]))}
 
     l1 = tf.add(tf.matmul(data, hidden_layer_1['weights']),  hidden_layer_1['biases'])
     l1 = tf.nn.relu(l1)
@@ -69,26 +69,26 @@ def neural_network_model(data):
 
 def train_neural_network(x):
     prediction = neural_network_model(x)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
     hm_epochs = 10
 
     with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
+        sess.run(tf.global_variables_initializer())
 
-        for epoch in hm_epochs:
+        for epoch in range(hm_epochs):
             epoch_loss = 0
             for _ in range(int(mnist.train.num_examples/batch_size)):
-                x, y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict={x: x, y: y})
-                epoch_loss = c
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                epoch_loss += c
             print('epoch', epoch, 'completed out of ', hm_epochs, 'loss', epoch_loss)
 
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
 
-        print('Accuracy', accuracy.eval({x: mnist.test.images, y: mnist.test.images}))
+        print('Accuracy', accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
 
 
 train_neural_network(x)
