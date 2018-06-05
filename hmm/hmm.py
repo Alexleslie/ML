8e9abb2e3a77d27ea1526455fa635a32b7b9dfdb
Alexleslie <577045156@qq.com>
2018-06-05 18:37:42 +0800

    update

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -1,15 +1,23 @@
 import numpy as np
 from sklearn.externals import joblib
+import random
+import pandas
+from urllib.parse import urlparse, unquote, parse_qsl
 
 with open('xss-train.txt', encoding='utf-8') as f:
-    content = f.readlines()
+    content = [i.strip('\n') for i in f.readlines()]
 
-with open('good-xss.txt', encoding='utf-8') as f:
-    good_query = f.readlines()
-    good_query = good_query[:2000]
 
-content = content + good_query
+good_query = pandas.read_csv('normal_examples.csv')
 
+
+good_query = ['php?'+ unquote(i) for i in good_query['url'] if (len(i) < 200) and (len(i)>1)]
+#
+# with open('good-xss.txt', encoding='utf-8') as f:
+#     good_query = [i.strip('\n') for i in f.readlines()]
+#     random.shuffle(good_query)
+#content = content + good_query
+random.shuffle(good_query)
 
 def et1(str):
     vers = []
@@ -23,23 +31,69 @@ def et1(str):
             vers.append([ord('C')])
     return np.array(vers)
 
+
+def deal(url):
+    from urllib.parse import urlparse, unquote, parse_qsl
+    result = urlparse(url)
+    query = unquote(result.query)
+    params = parse_qsl(query, True)
+    vers = []
+    for k, v in params:
+        vers.append(v)
+    return vers
+
+good = []
+bad = []
+
+for i in good_query:
+    if len(good) < 2000:
+        if deal(i):
+            good.append(deal(i))
+        else:
+            continue
+
+for i in content:
+    if len(bad) < 2000:
+        bad.append(deal(i))
+
+
+def conbine(url_list):
+    conbine_list = []
+    for i in url_list:
+        if len(i) <= 1:
+            conbine_list += i
+        else:
+            string = ''
+            for j in i:
+                string += j
+            conbine_list.append(string)
+    return conbine_list
+
+Good_conbine = conbine(good)
+Bad_conbine = conbine(bad)
+
+
 X = [[0]]
 X_lens = [1]
 
 y_good = [0 for _ in range(2000)]
 y_bad = [1 for _ in range(2000)]
 
+content = Good_conbine + Bad_conbine
+
 for i in content:
+    print(i)
     X = np.concatenate([X, et1(i)])
     X_lens.append(len(i))
 
 from hmmlearn import hmm
 
-# remodel = hmm.GaussianHMM(n_components=2, covariance_type='full', n_iter=100)
-# remodel.fit(X, X_lens)
+remodel = hmm.GaussianHMM(n_components=2, covariance_type='full', n_iter=100)
+remodel.fit(X, X_lens)
 
-# joblib.dump(remodel, 'hmm.m')
-# print('ok')
+joblib.dump(remodel, 'hmm.m')
+print('ok')
+input()
 
 remodel = joblib.load('hmm.m')
 start = 0
@@ -56,8 +110,7 @@ X_all = X_all[1:]
 y_all = y_bad + y_good
 
 prediction = []
-print(len(X_all))
-input()
+
 for i in X_all:
     score = remodel.score(i)
     if score < 300:
